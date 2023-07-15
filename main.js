788155b044c2b5cc95c592d3368cf818c26f7fde
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-07-15 12:53:08 +0300

    Added A High Quality Cover Image

--- main.js
+++ main.js
@@ -1,82 +1,8 @@
 /* Displaying Data */
 
-let data = {
-  kind: "books#volume",
-  id: "d225ywEACAAJ",
-  etag: "MzIDZkcWpdA",
-  selfLink: "https://www.googleapis.com/books/v1/volumes/d225ywEACAAJ",
-  volumeInfo: {
-    title: "Eat Sleep Robotics Repeat",
-    subtitle:
-      'Enthusiasts Gratitude Journal Planner 386 Pages Notebook Black Print 193 Days 8"x10" Thick Book',
-    authors: ["Family Cutey"],
-    publishedDate: "2019-11-06",
-    description:
-      'Family Cutey presents this cute and clean with lots of space, undated Gratitude Journal / Diary / Planner to keep or develop a positive attitude and a thankful mindset. contains: Date Statement pattern "Today I\'m grateful for__________" To Do List Chores Water Intake Meal Planner / Diary Notes Forget Me Nots Doodles Great gift for anyone who are organized, wants to be more organized, loves to doodle, loves documenting, writing.',
-    industryIdentifiers: [
-      {
-        type: "ISBN_10",
-        identifier: "1706176694",
-      },
-      {
-        type: "ISBN_13",
-        identifier: "9781706176695",
-      },
-    ],
-    readingModes: {
-      text: false,
-      image: false,
-    },
-    pageCount: 388,
-    printType: "BOOK",
-    maturityRating: "NOT_MATURE",
-    allowAnonLogging: false,
-    contentVersion: "preview-1.0.0",
-    panelizationSummary: {
-      containsEpubBubbles: false,
-      containsImageBubbles: false,
-    },
-    imageLinks: {
-      smallThumbnail:
-        "http://books.google.com/books/content?id=d225ywEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
-      thumbnail:
-        "http://books.google.com/books/content?id=d225ywEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-    },
-    language: "en",
-    previewLink:
-      "http://books.google.com.eg/books?id=d225ywEACAAJ&dq=intitle:repeat&hl=&cd=1&source=gbs_api",
-    infoLink:
-      "http://books.google.com.eg/books?id=d225ywEACAAJ&dq=intitle:repeat&hl=&source=gbs_api",
-    canonicalVolumeLink:
-      "https://books.google.com/books/about/Eat_Sleep_Robotics_Repeat.html?hl=&id=d225ywEACAAJ",
-  },
-  saleInfo: {
-    country: "EG",
-    saleability: "NOT_FOR_SALE",
-    isEbook: false,
-  },
-  accessInfo: {
-    country: "EG",
-    viewability: "NO_PAGES",
-    embeddable: false,
-    publicDomain: false,
-    textToSpeechPermission: "ALLOWED",
-    epub: {
-      isAvailable: false,
-    },
-    pdf: {
-      isAvailable: false,
-    },
-    webReaderLink:
-      "http://play.google.com/books/reader?id=d225ywEACAAJ&hl=&source=gbs_api",
-    accessViewStatus: "NONE",
-    quoteSharingAllowed: false,
-  },
-  searchInfo: {
-    textSnippet:
-      "Family Cutey presents this cute and clean with lots of space, undated Gratitude Journal / Diary / Planner to keep or develop a positive attitude and a thankful mindset. contains: Date Statement pattern &quot;Today I&#39;m grateful for__________&quot; To ...",
-  },
-};
+let data = {};
+
+let imageSrc = "";
 
 let authors = document.getElementById("authors");
 let title = document.getElementById("title");
@@ -89,7 +15,7 @@ let image = document.getElementById("preview");
 function Display() {
   if (!data) return Fetching();
 
-  image.setAttribute("src", data.volumeInfo.imageLinks.thumbnail);
+  image.setAttribute("src", imageSrc);
 
   if (data.volumeInfo.authors)
     authors.innerHTML = `Authors: ${data.volumeInfo.authors}`;
@@ -368,7 +294,7 @@ async function Fetching() {
   let result = await res.json();
   data = result.items[0];
 
-  console.log(data);
+  imageSrc = `https://books.google.com/books/publisher/content/images/frontcover/${data.id}?fife=w400-h630&source=gbs_api`;
 
   Display();
 }
